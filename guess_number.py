e6eab48f324f5eeada57dfcbed86a8269333a3d2
Kaleb Gwyn <gwynkaleb03@gmail.com>
2024-02-06 00:32:43 -0600

    Added cap on user guesses

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,16 @@
 number = '10'
+guess_count = 4
 
 print("I'm thinking of a number...")
-guess = input("What number am I thinking of? Enter 'q' to quit. ")
+guess = input(f"What number am I thinking of? You have {guess_count + 1} guesses. Enter 'q' to quit. ")
 
 while True:
-    if guess == 'q':
+    if guess == 'q' or guess_count == 0:
         print(f"Sorry! The number was {number}.")
         break
     elif guess == number:
         print("Congratulations! You guessed the right number.")
         break
     else:
-        guess = input("Try again. What number am I thinking of? Enter 'q' to quit. ")
+        guess = input(f"Try again. What number am I thinking of? You have {guess_count} guesses left. Enter 'q' to quit. ")
+        guess_count -= 1
